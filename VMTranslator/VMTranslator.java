cc1c3e795da9a1915097de5b2d5bde1d420b1e65
Ryan Jansen <rjgiga444@gmail.com>
2020-12-03 14:38:26 +0800

    Worked on project 8

--- VMTranslator/VMTranslator.java
+++ VMTranslator/VMTranslator.java
@@ -7,7 +7,13 @@ public class VMTranslator {
     enum CTYPE {
         C_ARITHMETIC,
         C_PUSH,
-        C_POP
+        C_POP,
+        C_GOTO,
+        C_IF,
+        C_LABEL,
+        C_FUNCTION,
+        C_RETURN,
+        C_CALL
     }
 
     private static class Command {
@@ -68,6 +74,25 @@ public class VMTranslator {
                     command = new Command(CTYPE.C_PUSH, cmd[1], Integer.parseInt(cmd[2]));
                 }
 
+                // Branching Commands
+                if (cmd[0].equals("label")) {
+                    command = new Command(CTYPE.C_LABEL, cmd[1], 0);
+                } else if (cmd[0].equals("goto")) {
+                    command = new Command(CTYPE.C_GOTO, cmd[1], 0);
+                } else if (cmd[0].equals("if-goto")) {
+                    command = new Command(CTYPE.C_IF, cmd[1], 0);
+                }
+
+                // Function Commands
+                if (cmd[0].equals("function")) {
+                    command = new Command(CTYPE.C_FUNCTION, cmd[1],Integer.parseInt(cmd[2]));
+                } else if (cmd[0].equals("return")) {
+                    command = new Command(CTYPE.C_RETURN, "", 0);
+                }  else if (cmd[0].equals("call")) {
+                    command = new Command(CTYPE.C_CALL, cmd[1], Integer.parseInt(cmd[2]));
+                }
+                
+
             }
            
         }
@@ -113,7 +138,7 @@ public class VMTranslator {
 
         public void writeArithmetic(String cmd){
             p.println("// " + cmd + " command");
-            p.println("@R0"); // Access stack pointer
+            p.println("@SP"); // Access stack pointer
             
             if (cmd.equals("not")) {
                 p.println("A=M-1");
@@ -124,7 +149,7 @@ public class VMTranslator {
             } else {
                 p.println("AM=M-1"); // Point to top element of stack and go there
                 p.println("D=M"); // Get y
-                p.println("@R0");
+                p.println("@SP");
                 p.println("A=M-1"); // Get x
             }
 
@@ -151,13 +176,13 @@ public class VMTranslator {
                     p.println("D;JLT");
                 }
 
-                p.println("@R0");
+                p.println("@SP");
                 p.println("A=M-1");
                 p.println("M=0");
                 p.println("@END:" + n); 
                 p.println("0;JMP");
                 p.println("(TRUE:" + n + ")");
-                p.println("@R0");
+                p.println("@SP");
                 p.println("A=M-1");
                 p.println("M=-1");
                 p.println("(END:" + n + ")");
@@ -189,13 +214,13 @@ public class VMTranslator {
                 }else {
 
                     if (segment.equals("local")) {
-                        p.println("@R1");
+                        p.println("@LCL");
                     } else if (segment.equals("argument")) {
-                         p.println("@R2");
+                         p.println("@ARG");
                     } else if (segment.equals("this")) {
-                        p.println("@R3");
+                        p.println("@THIS");
                     } else if (segment.equals("that")) {
-                        p.println("@R4");
+                        p.println("@THIS");
                     }
                         p.println("D=M");
                         p.println("@" + i);
@@ -206,29 +231,29 @@ public class VMTranslator {
 
                 
 
-                p.println("@R0");
+                p.println("@SP");
                 p.println("A=M");
                 p.println("M=D");
-                p.println("@R0");
+                p.println("@SP");
                 p.println("M=M+1");
             
             
         } else if (cmd == CTYPE.C_POP) {
             p.println("// Pop " + segment + " " + i);
             if (segment.equals("temp")) { 
-                p.println("@R0");
+                p.println("@SP");
                 p.println("AM=M-1");
                 p.println("D=M");
                 p.println("@" + Integer.toString(5+index));
                 p.println("M=D");
             } else if (segment.equals("pointer")) {
-                p.println("@R0");
+                p.println("@SP");
                 p.println("AM=M-1");
                 p.println("D=M");
                 p.println("@" + Integer.toString(3+index));
                 p.println("M=D");
             } else if (segment.equals("static")) {
-                p.println("@R0");
+                p.println("@SP");
                 p.println("AM=M-1");
                 p.println("D=M");
                 p.println("@" + fileName + "." + i);
@@ -237,13 +262,13 @@ public class VMTranslator {
             } else {
                 
                 if (segment.equals("local")) {
-                    p.println("@R1");
+                    p.println("@LCL");
                 } else if (segment.equals("argument")) {
-                    p.println("@R2");
+                    p.println("@ARG");
                 } else if (segment.equals("this")) {
-                    p.println("@R3");
+                    p.println("@THIS");
                 } else if (segment.equals("that")) {
-                    p.println("@R4");
+                    p.println("@THAT");
                 }
                 
                 p.println("D=M");
@@ -251,7 +276,7 @@ public class VMTranslator {
                 p.println("D=D+A");
                 p.println("@R13");
                 p.println("M=D");
-                p.println("@R0");
+                p.println("@SP");
                 p.println("AM=M-1");
                 p.println("D=M");
                 p.println("@R13");
@@ -261,12 +286,158 @@ public class VMTranslator {
 
             }
         }
+        
+        public void writeInit() {
+            p.println("@256");
+            p.println("D=A");
+            p.println("@SP");
+            p.println("M=D");
+            writeCall("Sys.init", 0);
+        }
+            
+        public void writeLabel(String label) {
+            p.println("(" + label + ")");
+        }
+
+        public void writeGoto(String label) {
+            p.println("@" + label);
+            p.println("0;JMP");
+        }
+
+        public void writeIf(String label) {
+            p.println("@SP");
+            p.println("AM=M-1");
+            p.println("D=M");
+            p.println("@" + label);
+            p.println("D;JNE");
+        }
+
+        public void writeCall(String fnName, int numArgs) {
+            p.println("@ret:" + Integer.toString(r)); // Push Return Address
+            p.println("D=A");
+            p.println("@SP");
+            p.println("A=M");
+            p.println("M=D");
+            p.println("@SP");
+            p.println("M=M+1");
+            
+            p.println("@LCL"); // Save LCL of caller
+            p.println("D=M");
+            p.println("@SP");
+            p.println("A=M");
+            p.println("M=D");
+            p.println("@SP");
+            p.println("M=M+1"); 
+        
+            p.println("@ARG"); // Save ARG of caller
+            p.println("@SP");
+            p.println("A=M");
+            p.println("M=D");
+            p.println("@SP");
+            p.println("M=M+1");
+
+            p.println("@THIS"); // Save THIS of caller
+            p.println("@SP");
+            p.println("A=M");
+            p.println("M=D");
+            p.println("@SP");
+            p.println("M=M+1");
+
+            p.println("@THAT"); // Save THAT of caller
+            p.println("@SP");
+            p.println("A=M");
+            p.println("M=D");
+            p.println("@SP");
+            p.println("M=M+1");
+            p.println("D=M");
+            
+            p.println("@" + Integer.toString(numArgs)); // Reposition ARG
+            p.println("D=D-A");
+            p.println("@5");
+            p.println("D=D-A");
+            p.println("@ARG");
+            p.println("M=D");
+            
+
+            p.println("@SP"); // Reposition LCL
+            p.println("D=M");
+            p.println("@LCL");
+            p.println("M=D");
+
+            writeGoto(fnName); // Go to callee
+
+            p.println("@ret:" + Integer.toString(r)); // Set return address
+
+            r += 1;
+        }
+
+        public void writeReturn() {
+            p.println("@LCL"); // Save LCL in R13
+            p.println("D=M");
+            p.println("@R13");
+            p.println("M=D");
+
+            p.println("@5"); // Save return address in R14
+            p.println("D=D-A");
+            p.println("A=D");
+            p.println("D=M");
+            p.println("@R14");
+            p.println("M=D");
+
+            p.println("@SP"); // Reposition return value
+            p.println("AM=M-1");
+            p.println("D=M");
+            p.println("@ARG");
+            p.println("A=M");
+            p.println("M=D");
+
+            p.println("D=A+1"); // Restore SP of caller
+            p.println("@SP");
+            p.println("M=D");
+
+            p.println("@R13"); // Restore THAT of caller
+            p.println("AM=M-1");
+            p.println("D=M");
+            p.println("@THAT");
+            p.println("M=D"); 
+
+            p.println("@R13"); // Restore THIS of caller
+            p.println("AM=M-1");
+            p.println("D=M");
+            p.println("@THIS");
+            p.println("M=D");
+
+            p.println("@R13"); // Restore ARG of caller
+            p.println("AM=M-1");
+            p.println("D=M");
+            p.println("@ARG");
+            p.println("M=D");
+            
+            p.println("@R13"); // Restore LCL of caller
+            p.println("AM=M-1");
+            p.println("D=M");
+            p.println("@LCL");
+            p.println("M=D");
+
+            p.println("@R14"); // Go to return address in caller
+            p.println("A=M");
+            p.println("0;JMP");
+
+        }
+
+        public void writeFunction(String fnName, int numLocals) {
+            p.println("(" + fnName + ")");
+            for (int i = 0; i < numLocals; i++) {
+                writePushPop(CTYPE.C_PUSH, "constant", 0);
+            }
+        }
 
         public void close(){
             p.close();
         }
 
         private static PrintWriter p;
+        private static int r = 0; 
 
     }
 
@@ -280,13 +451,28 @@ public class VMTranslator {
         String outputName = fileName.substring(0, fileName.length() - 3);
         Parser p = new Parser(new FileInputStream(fileName));
         CodeWriter c = new CodeWriter(new FileOutputStream(outputName + ".asm"));
+        String currentFunction = "";
         c.setFileName(outputName); 
+        c.writeInit();
         while (p.hasMoreCommands()) {
             p.advance();
             if (p.commandType() == CTYPE.C_ARITHMETIC) {
                 c.writeArithmetic(p.arg1());
             } else if (p.commandType() == CTYPE.C_POP || p.commandType() == CTYPE.C_PUSH) {
                 c.writePushPop(p.commandType(), p.arg1(), p.arg2());
+            } else if (p.commandType() == CTYPE.C_FUNCTION) {
+                currentFunction = p.arg1();
+                c.writeFunction(p.arg1(), p.arg2());
+            } else if (p.commandType() == CTYPE.C_CALL) {
+                c.writeCall(p.arg1(), p.arg2());
+            } else if (p.commandType() == CTYPE.C_RETURN) {
+                c.writeReturn();
+            } else if (p.commandType() == CTYPE.C_IF) {
+                c.writeIf(currentFunction + "$" + p.arg1());
+            } else if (p.commandType() == CTYPE.C_GOTO) {
+                c.writeGoto(currentFunction + "$" + p.arg1());
+            } else if (p.commandType() == CTYPE.C_LABEL) {
+                c.writeLabel(currentFunction + "$" + p.arg1());
             }
         }
 
